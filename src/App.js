0194994f610d937a7afb98d9eee51a4d53fb6d41
prathamesh9164 <prathameshtalele1@gmail.com>
2025-03-16 15:58:55 +0530

    gfgf

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
-import About from "./components/About";
+// import About from "./components/About";
 import React, { useState } from "react";
 import Alert from "./components/Alert";
-import { BrowserRouter as Router, Switch, Route, Routes, Link } from "react-router-dom";
+// import { BrowserRouter as Router, Route, Routes, } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState("light"); // Whether dark mode is enabled or not
@@ -33,31 +33,31 @@ function App() {
   };
   return (
     <>
-      <Router>
-        <Navbar
-          title="TextUtils"
-          mode={mode}
-          toggleMode={toggleMode}
-          key={new Date()}
-        />
-        <Alert alert={alert} />
-        <div className="container my-3">
-          <Routes>
-            {/* /users --> Component 1
+      {/* <Router> */}
+      <Navbar
+        title="TextUtils"
+        mode={mode}
+        toggleMode={toggleMode}
+        key={new Date()}
+      />
+      <Alert alert={alert} />
+      <div className="container my-3">
+        {/* <Routes> */}
+        {/* /users --> Component 1
         /users/home --> Component 2 */}
-            <Route exact path="/about"
+        {/* <Route exact path="/about"
               element={<About mode={mode} />}
-             />
-            <Route exact path="/"
-              element={<TextForm
-                showAlert={showAlert}
-                heading="Try TextUtils - word counter, character counter, remove extra spaces"
-                mode={mode}
-              />}
-            />
-          </Routes>
-        </div>
-      </Router>
+             /> */}
+        {/* <Route exact path="/"> */}
+        <TextForm
+          showAlert={showAlert}
+          heading="Try TextUtils - word counter, character counter, remove extra spaces"
+          mode={mode}
+        />
+        {/* <Route/> */}
+        {/* </Routes> */}
+      </div>
+      {/* </Router> */}
     </>
   );
 }
